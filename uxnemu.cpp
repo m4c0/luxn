6f54a5433bcaa08f9e860478b839a1ca97230335
Eduardo Costa <m4c0@users.noreply.github.com>
2024-04-09 12:55:25 +0100

    uxnemu: everything but SDL

---
+++ uxnemu.cpp
@@ -0,0 +1,10 @@
+#pragma leco tool
+#pragma leco add_impl "uxn/src/uxn.c"
+#pragma leco add_impl "uxn/src/devices/system.c"
+#pragma leco add_impl "uxn/src/devices/console.c"
+#pragma leco add_impl "uxn/src/devices/file.c"
+#pragma leco add_impl "uxn/src/devices/datetime.c"
+#pragma leco add_impl "uxn/src/devices/mouse.c"
+#pragma leco add_impl "uxn/src/devices/controller.c"
+#pragma leco add_impl "uxn/src/devices/screen.c"
+#pragma leco add_impl "uxn/src/devices/audio.c"

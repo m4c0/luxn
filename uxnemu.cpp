0e4d1081436b1c5e2bcc098cec65e93f797f9b95
Eduardo Costa <m4c0@users.noreply.github.com>
2024-04-10 10:44:46 +0100

    emu: pulling a "quack" boilerplate

--- uxnemu.cpp
+++ uxnemu.cpp
@@ -8,3 +8,53 @@
 #pragma leco add_impl "uxn/src/devices/controller.c"
 #pragma leco add_impl "uxn/src/devices/screen.c"
 #pragma leco add_impl "uxn/src/devices/audio.c"
+
+import casein;
+import quack;
+import vee;
+import voo;
+
+class thread : public voo::casein_thread {
+  void run() override {
+    voo::device_and_queue dq{"uxnemu", native_ptr()};
+    quack::pipeline_stuff ps{dq, 1};
+    quack::instance_batch ib{ps.create_batch(1)};
+    ib.map_all([](auto p) {
+      auto &[cs, ms, ps, us] = p;
+      ps[0] = {{0, 0}, {1, 1}};
+      cs[0] = {0, 0, 0, 1};
+      us[0] = {{0, 0}, {1, 1}};
+      ms[1] = {1, 1, 1, 1};
+    });
+
+    voo::h2l_image a{dq.physical_device(), 32, 32};
+    auto smp = vee::create_sampler(vee::nearest_sampler);
+    auto dset = ps.allocate_descriptor_set(a.iv(), *smp);
+
+    quack::upc rpc{
+        .grid_pos = {0.5f, 0.5f},
+        .grid_size = {1.0f, 1.0f},
+    };
+
+    while (!interrupted()) {
+      voo::swapchain_and_stuff sw{dq};
+      extent_loop(dq.queue(), sw, [&] {
+        auto upc = quack::adjust_aspect(rpc, sw.aspect());
+        sw.queue_one_time_submit(dq.queue(), [&](auto pcb) {
+          auto scb = sw.cmd_render_pass(pcb);
+          vee::cmd_set_viewport(*scb, sw.extent());
+          vee::cmd_set_scissor(*scb, sw.extent());
+          ib.build_commands(*pcb);
+          ps.cmd_bind_descriptor_set(*scb, dset);
+          ps.cmd_push_vert_frag_constants(*scb, upc);
+          ps.run(*scb, 1);
+        });
+      });
+    }
+  }
+};
+
+extern "C" void casein_handle(const casein::event &e) {
+  static thread t{};
+  t.handle(e);
+}
